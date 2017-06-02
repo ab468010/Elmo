c4304f08e1a5a2fb185c55aab3d1288cf49df062
ElmoZhong <ElmoZhong@DESKTOP-E8N3QUO>
2017-06-02 16:01:45 +0800

    初始化项目

---
+++ Elmo.Business/Base/Db.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dos.ORM;
+
+namespace Elmo.Business.Base
+{
+    public class Db
+    {
+        public static readonly DbSession Context = new DbSession("Conn");
+
+        static Db()
+        {
+            Context.RegisterSqlLogger(delegate (string sql) 
+            {
+
+            } );
+        }
+    }
+}
