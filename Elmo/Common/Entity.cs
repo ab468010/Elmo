df6ca0abe96c60fed7826918b44ad4395eb0e689
ElmoZhong <ElmoZhong@DESKTOP-E8N3QUO>
2017-06-02 11:23:08 +0800

    初始化

---
+++ Elmo/Common/Entity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elmo.Common
+{
+    public class Entity
+    {
+        /// <summary>
+        /// GetFields
+        /// </summary>
+        /// <returns></returns>
+        public virtual Field[] GetFields() { return null; }
+        /// <summary>
+        /// GetValues
+        /// </summary>
+        /// <returns></returns>
+        public virtual object[] GetValues() { return null; }
+    }
+
+
+    /// <summary>
+    /// 标记实体类表名
+    /// </summary>
+    public class Table : Attribute
+    {
+        private string _tableName;
+        private string _userName;
+
+        public Table(string tableName)
+        {
+            this._tableName = tableName;
+        }
+
+        public Table(string tableName, string username)
+        {
+            this._tableName = tableName;
+            this._userName = username;
+        }
+
+        public string GetTableName()
+        {
+            return _tableName;
+        }
+
+        public string GetUserName()
+        {
+            return _userName;
+        }
+
+
+
+
+    }
+}
---
+++ Elmo/ORM/MODEL.cs
@@ -0,0 +1,38 @@
+﻿using Elmo.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elmo.Common;
+
+namespace Elmo.ORM
+{
+
+    [Table("Test")]
+    [Serializable]
+    public class MODEL:Entity
+    {
+        [Field("ProductId","Product")]
+        public string ProductId { get; set; }
+
+
+        /// <summary>
+        /// 获取列信息
+        /// </summary>
+        public override Field[] GetFields()
+        {
+            return new Field[] {
+               
+            };
+        }
+
+        public Field[] GetFields<TFiled>()
+        {
+            return new Field[] {
+
+            };
+        }
+
+    }
+}
